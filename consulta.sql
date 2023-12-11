e70125d922297f1683b4ca2d0ad4e447b4a73cb3
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2023-12-11 12:48:07 -0300

    .

---
+++ consulta.sql
@@ -0,0 +1,10 @@
+use dw_vista
+SELECT T.[Status], T.Nome, T.TerminoReal, R.Recurso as Supervisor, Es.Descricao as 'Local' 
+from FT_TAREFA T
+inner join DM_ESTRUTURA Es on Es.Id_Estrutura = T.Id_Estrutura
+inner join FT_CHECKLIST_RESPOSTA_FULL R on R.TarefaId = T.Id
+inner join DM_SERVICO Sv on Sv.Id_Servico = T.Id_Servico
+where Es.CRno = 42636
+and Sv.Servico LIKE '%VISITA%'
+and MONTH(TerminoReal) = 8
+ORDER BY TerminoReal DESC
