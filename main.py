5bd6d765320a9763dee8dcb6c05a33073bedf236
foxtec198 <64221923+foxtec198@users.noreply.github.com>
2023-12-13 18:19:44 -0300

    .

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ class Consulta():
     def __init__(self):
         self.xSenha = 0
         self.numeroDeTentativas = 3
-
-    def db(self):
-        ...
         
     def dados(self):
         ...
+
+    def DB(self):
+        self.conex = connect(f"DRIVER = SQL Driver;SERVER = {self.server}; UID = {self.user}; PWD = {self.pwd}")
+        self.c = self.conex.cursor()
         
     def login(self):
         user = input('user: ')
@@ -20,6 +21,7 @@ class Consulta():
         r = requests.get('https://db-geradorqr-default-rtdb.firebaseio.com/.json')
         d = json.loads(r.text)
         d = d['login']
+        
         for i in d:
             users = d[i]
             if user == users['user']:
@@ -29,9 +31,10 @@ class Consulta():
                 if self.pwd == passw:
                     self.xSenha = 0
                     print('Logado com sucesso')
+                    self.DB()
                     break
                 else:
-                    if self.xSenha == 3:
+                    if self.xSenha == 2: # 3x é o numero de tentativas
                         print('Numero de tentativas execedidas, tente redefenir sua senha')
                         break
                     else:
@@ -42,27 +45,28 @@ class Consulta():
                         self.login()
                     break
             else:
-                print('Usuário Incorreto!') 
-                self.login()
+                print('Usuário Incorreto')
         
-    def cadastroUser(self, user, pwd, server):
-        d = {'user':user, 'server':server, 'pwd':pwd}
+    def cadastroUser(self, userP, pwd, server):
+        d = {'user':userP, 'server':server, 'pwd':pwd}
         dados = requests.get('https://db-geradorqr-default-rtdb.firebaseio.com/.json')
         dados = json.loads(dados.text)
         dados = dados['login']
-
+        
+        self.totalDeUsuarios = len(dados)
+        self.contx = 0
         for i in dados:
             key = dados[i]
-            if user == key['user']:
-                # LOGIN ATUALIZADO 
-                r = requests.patch(f'https://db-geradorqr-default-rtdb.firebaseio.com/login/{i}/.json', json=d)
-                print(r.text)
+            self.contx += 1
+            if userP == key['user']:
+                r = requests.patch(f'https://db-geradorqr-default-rtdb.firebaseio.com/login/{i}.json', json=d)
+                print('LOGIN ATUALIZADO')
                 break
-            # else:
-                # LOGIN REALIZADO
-                # r = requests.post(f'https://db-geradorqr-default-rtdb.firebaseio.com/login.json', json=d)
-                # break
-        
+            elif self.contx == self.totalDeUsuarios:
+                r = requests.post(f'https://db-geradorqr-default-rtdb.firebaseio.com/login.json', json=d)
+                print('LOGIN REALIZADO')
+                break
+
 c = Consulta()
         
-c.cadastroUser(user='testeAdd', pwd='Tetse', server='10.56.6.56')
+c.login()
