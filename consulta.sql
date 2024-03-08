ed160d981a702d4ab7170039f64901542ab89145
Guilherme Breve <foxtec198@gmail.com>
2024-03-08 12:14:13 -0300

    .

--- consulta.sql
+++ consulta.sql
@@ -1,11 +1,10 @@
-select R.Nome as Sup, COUNT(R.Nome)  as Realizado, DAY(TerminoReal) as Dia
+select
+T.Nome, R.nome, T.TerminoReal as "Horario", Es.Descricao as 'local', EstruturaHierarquiaDescricao as 'Local completo'
 from Tarefa T
-inner join Recurso R on R.CodigoHash = T.FinalizadoPorHash
-inner join dw_vista.dbo.DM_Estrutura Es on Es.Id_Estrutura = T.EstruturaId
-inner join dw_vista.dbo.DM_CR c on c.Id_cr = Es.Id_cr
-where c.Gerente = 'DENISE DOS SANTOS DIAS SILVA'
-and T.Nome LIKE '%Visita %' 
-and month(Disponibilizacao) = 01
-and YEAR(T.Disponibilizacao) = 2024
-GROUP BY R.Nome, DAY(TerminoReal)
-ORDER BY DAY(TerminoReal)
+join Estrutura Es on T.EstruturaId = Es.Id
+join Recurso R on R.CodigoHash = T.FinalizadoPorHash
+where EstruturaHierarquiaDescricao LIKE '%42610 %'
+and T.Nome LIKE '%RONDA %'
+and MONTH(TerminoReal) >= 01 
+and MONTH(TerminoReal) <= 02 
+and YEAR(TerminoReal) = 2024 
