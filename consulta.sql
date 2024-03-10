0841294d3267dbe228d0a9002ba962b223757dd7
Guilherme Breve <foxtec198@gmail.com>
2024-03-10 10:38:02 -0300

    .

--- consulta.sql
+++ consulta.sql
@@ -1,10 +1,38 @@
-select
-T.Nome, R.nome, T.TerminoReal as "Horario", Es.Descricao as 'local', EstruturaHierarquiaDescricao as 'Local completo'
-from Tarefa T
-join Estrutura Es on T.EstruturaId = Es.Id
-join Recurso R on R.CodigoHash = T.FinalizadoPorHash
-where EstruturaHierarquiaDescricao LIKE '%42610 %'
-and T.Nome LIKE '%RONDA %'
-and MONTH(TerminoReal) >= 01 
-and MONTH(TerminoReal) <= 02 
-and YEAR(TerminoReal) = 2024 
+-- select 
+-- T.Id,
+-- T.ChecklistId,
+-- T.Nome,
+-- T.Descricao,
+-- T.Numero,
+-- T.Escalonado,
+-- T.Prazo,
+-- T.Disponibilizacao,
+-- T.InicioReal,
+-- T.TerminoReal,
+-- T.ServicoDescricao,
+-- R.Nome as 'Colaborador',
+-- Es.Descricao as 'Local',
+-- Es.HierarquiaDescricao,
+-- E.Longitude,
+-- E.Latitude,
+-- E.PerguntaDescricao as 'Pergunta',
+-- E.Conteudo as 'Resposta'
+-- from Tarefa T
+-- inner join Execucao E
+--     on E.TarefaId = T.Id
+-- inner join Recurso R
+--     on R.CodigoHash = T.FinalizadoPorHash
+-- inner join Estrutura Es
+--     on Es.Id = T.EstruturaId
+-- where 
+--     T.EstruturaNivel1 LIKE '%189535 -%'
+-- and
+--     T.ChecklistId IN (
+--         'd93f3b97-e34b-4d6c-8912-64ea8c4f7856',
+--         '3db0310c-7827-4fdc-ae74-751e5af7c6ac',
+--         'ccba2b1e-5ab7-4ec9-9912-f0365b466203'
+--     )
+
+
+select top 1 T.EstruturaNivel1 from Tarefa T
+where T.EstruturaNivel1 LIKE '%189535 -%'
